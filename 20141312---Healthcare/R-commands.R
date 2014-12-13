790c3c7538828952f2dbabbbf39ee41f130f29d2
Alex <AexIG@users.noreply.github.com>
2014-12-13 19:07:59 +0000

    Updated file.

--- 20141312---Healthcare/R-commands.R
+++ 20141312---Healthcare/R-commands.R
@@ -1,16 +1,18 @@
 #Initial bit of data munging (remove NAs)
-setwd('west-africa-ebola-outbreak/')
+#setwd('Personal/HackDays/20141213 - Healthcare/west-africa-ebola-outbreak/')
 flights <- read.csv('Data/flights.csv')
 newFlights <- filter(flights, complete.cases(flights) == TRUE)
 dim(newFlights)
 
 #Use ebola data to try and plot an interactive plot showing the values
 #with date as our interactive column.
+library(reshape2)
+library(googleVis)
 ebola_data <- read.csv('Data//ebola-data-db-format.csv')
-ebola_data <- data.table(ebola_data)
+#ebola_data <- data.table(ebola_data)
 ebola_data$Date <- as.Date(ebola_data$Date)
 #Data is in long form, need it in wide
-library(reshape2)
+#library(reshape2)
 ebola_wide <- dcast(ebola_data, Country + Date ~ Indicator)
 ebola_wide2 <- ebola_wide
 #Fill in missing values with zeroes.
@@ -20,23 +22,23 @@ ebola_wide2[which(is.na(ebola_wide), arr.ind = TRUE)] = 0
 #Want different colours to represent different counties, but idvar needs to
 #be set to country as well, which seemed to cause problems!
 #Create a dummy variable for country to be used
-ebola_wide$country2 <- ebola_wide$Country
+ebola_wide2$country2 <- ebola_wide$Country
 M <- gvisMotionChart(ebola_wide2, idvar='country2', timevar='Date', 
                      xvar = 'Case fatality rate (CFR) of confirmed Ebola cases', 
                      yvar = 'Number of confirmed Ebola cases in the last 21 days', 
                      colorvar = 'Country')
 plot(M)
 
-#Let's look more closely at data relating to Guinea
-medicine <- read.csv('Data/2014-12-03.csv')
-#First thing is that epi.week cannot be used
-#Split in the word 'to'
-date_breakdown <- str_split(medicine$Epi.week, 'to')
-source('date_sync.R')
-medicine$date_from <- as.Date(sapply(date_breakdown, date_synchronisation), format = '%d %B %Y')
-medicine$date_to <- as.Date(sapply(date_breakdown,function(x) x[2]), format = '%d %B %Y')
-
 # #===================================================================
+# #Let's look more closely at data relating to Guinea
+# medicine <- read.csv('Data/2014-12-03.csv')
+# #First thing is that epi.week cannot be used
+# #Split in the word 'to'
+# date_breakdown <- str_split(medicine$Epi.week, 'to')
+# source('date_sync.R')
+# medicine$date_from <- as.Date(sapply(date_breakdown, date_synchronisation), format = '%d %B %Y')
+# medicine$date_to <- as.Date(sapply(date_breakdown,function(x) x[2]), format = '%d %B %Y')
+# 
 # #Attempted to replicate plot from above, but failed. Look at later
 # #Use same thing as before
 # #Turn into wide format

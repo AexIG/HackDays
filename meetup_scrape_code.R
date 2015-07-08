d9227825fef53f9f90a30a37650090f8dcf7594b
Alex <alexiglaser@users.noreply.github.com>
2015-07-08 19:49:33 +0100

    Update meetup_scrape_code.R

--- meetup_scrape_code.R
+++ meetup_scrape_code.R
@@ -1,6 +1,10 @@
 library(XML)
 library(RCurl)
 library(RSelenium)
+library(plyr); library(dplyr)
+library(gtools)
+checkForServer()
+startServer()
 link <- "http://www.meetup.com/London-Kaggle-Meetup/"
 remDr <- remoteDriver(browserName = 'firefox')
 remDr$open(silent = TRUE)
@@ -11,7 +15,7 @@ htmlTree <- htmlParse(remDr$getPageSource()[[1]])
 name <- xpathSApply(htmlTree, "//a[@class='memName']", xmlValue)
 href <- xpathSApply(htmlTree, "//a[@class='memName']/@href")
 totalPages <- xpathSApply(htmlTree, "//li[contains(@class, 'nav-pageitem')]/a", xmlValue) %>% head(-1) %>% as.numeric %>% max
-newLink <- "http://www.meetup.com/London-Kaggle-Meetup/members/?offset=%d&sort=last_visited&desc=1   "
+newLink <- "http://www.meetup.com/London-Kaggle-Meetup/members/?offset=%d&sort=last_visited&desc=1"
 for (page in 2:totalPages)
 {
   link <- sprintf(newLink, (page-1)*20)
@@ -20,4 +24,26 @@ for (page in 2:totalPages)
   name <- c(name, xpathSApply(htmlTree, "//a[@class='memName']", xmlValue))
   href <- c(href, xpathSApply(htmlTree, "//a[@class='memName']/@href"))
 }
-df <- data.frame(name = name, href = href) %>% unique
+df <- data.frame(id = 1:length(name), name = name, href = href) %>% unique
+df$intro = ''
+listInfo <- list()
+#for (i in 1:nrow(df))
+for (i in 9:nrow(df))
+{
+  remDr$navigate(df$href[i])
+  Sys.sleep(4)
+  htmlTree <- htmlParse(remDr$getPageSource()[[1]])
+  moreGroups <- xpathSApply(htmlTree, "//a[@id='see-more-groups-toggle']", xmlValue)
+  if (!invalid(moreGroups))
+  {
+   remDr$findElement(using='xpath', "//a[@id='see-more-groups-toggle']")$clickElement()
+   meetups <- xpathSApply(htmlTree, "//div[@class='D_name']/a", xmlValue)
+   tryCatch({
+     intro <- xpathSApply(htmlTree, "//div[@class='D_memberProfileContentItem']/p", xmlValue) %>%
+       last
+     df$intro[i] = intro
+   },error = function(e){x <- 1:10}
+  )
+  listInfo[[i]] <- data.frame(id = df$id[i], meetups = meetups)
+  }
+}

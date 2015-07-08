a18caf80179e9a011f384e548761029c9e18e40b
Alex <alexiglaser@users.noreply.github.com>
2015-07-08 20:02:09 +0100

    Update meetup_scrape_code.R

--- meetup_scrape_code.R
+++ meetup_scrape_code.R
@@ -1,20 +1,26 @@
+# Read in libraries used. Note that RSelenium has been loaded up via github
 library(XML)
 library(RCurl)
 library(RSelenium)
 library(plyr); library(dplyr)
 library(gtools)
-checkForServer()
+# Run these commands to set up the Selenium server
+# checkForServer() # Only need to run this once when we first start
 startServer()
 link <- "http://www.meetup.com/London-Kaggle-Meetup/"
 remDr <- remoteDriver(browserName = 'firefox')
 remDr$open(silent = TRUE)
 remDr$navigate(link)
+# Click on button to find members of group
 try(elem <- remDr$findElement(using = 'xpath', value = "//a[contains(@href, 'London-Kaggle-Meetup/members')]"))
 elem$clickElement()
 htmlTree <- htmlParse(remDr$getPageSource()[[1]])
+# Get their names and web urls
 name <- xpathSApply(htmlTree, "//a[@class='memName']", xmlValue)
 href <- xpathSApply(htmlTree, "//a[@class='memName']/@href")
+# Note head(x, -1) removes the last element, which is an arrow
 totalPages <- xpathSApply(htmlTree, "//li[contains(@class, 'nav-pageitem')]/a", xmlValue) %>% head(-1) %>% as.numeric %>% max
+# Now loop over all available pages to complete the list
 newLink <- "http://www.meetup.com/London-Kaggle-Meetup/members/?offset=%d&sort=last_visited&desc=1"
 for (page in 2:totalPages)
 {
@@ -24,11 +30,12 @@ for (page in 2:totalPages)
   name <- c(name, xpathSApply(htmlTree, "//a[@class='memName']", xmlValue))
   href <- c(href, xpathSApply(htmlTree, "//a[@class='memName']/@href"))
 }
-df <- data.frame(id = 1:length(name), name = name, href = href) %>% unique
+df <- data.frame(id = 1:length(name), name = name, href = href, stringsAsFactors = FALSE) %>% unique
+
+# Loop overall the urls, collecting the meetups groups in a big list
 df$intro = ''
 listInfo <- list()
-#for (i in 1:nrow(df))
-for (i in 9:nrow(df))
+for (i in 1:nrow(df))
 {
   remDr$navigate(df$href[i])
   Sys.sleep(4)
@@ -47,3 +54,4 @@ for (i in 9:nrow(df))
   listInfo[[i]] <- data.frame(id = df$id[i], meetups = meetups)
   }
 }
+remDr$close()
